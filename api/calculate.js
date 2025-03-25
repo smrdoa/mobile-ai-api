66ee0be38d6d3b6b6bad998c77ef78b98785819f
smrdoa <lizaryatextile@gmail.com>
2025-03-25 04:22:16 +0300

    Update calculate.js

--- api/calculate.js
+++ api/calculate.js
@@ -1,6 +1,12 @@
-// api/calculate.js
-
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
